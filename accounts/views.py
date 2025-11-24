5cfb54ddf8be7b8356129a1a21b997189c7479da
Sandesh <sandeshacharya471@gmail.com>
2025-11-25 00:02:29 +0545

    Fix custom command location

---
+++ accounts/views.py
@@ -0,0 +1,10 @@
+# accounts/views.py
+from django.http import HttpResponse
+from django.contrib.auth import get_user_model
+
+def init_railway_admin(request):
+    User = get_user_model()
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser("admin", "admin@kishan.com", "KishanSecure2025!")
+        return HttpResponse("✅ Superuser created in Railway DB!")
+    return HttpResponse("⚠️ Already exists.")
